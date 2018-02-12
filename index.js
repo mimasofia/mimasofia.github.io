0ca884639c864a9989d96d85701ddb866aefcf3d
mimasofia <virginiadonaire@hotmail.com>
2018-02-12 16:24:57 -0500

    Add different colors to html, add smooth scroll
    Added different colors to the sections so one section has a color and
    the next one is white. Added smooth scrolling from nav to desired
    section

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 $(() => {
 
 })
+
+$('.about-scroll').click(function () {
+  $('html, body').animate({
+    scrollTop: $('.about-me').offset().top
+  }, 1000)
+})
+
+$('.current-skill-scroll').click(function () {
+  $('html, body').animate({
+    scrollTop: $('.skills').offset().top
+  }, 1000)
+})
+
+$('.project-scroll').click(function () {
+  $('html, body').animate({
+    scrollTop: $('.project').offset().top
+  }, 1000)
+})
+
+$('.to-top-scroll').click(function () {
+  $('html, body').animate({
+    scrollTop: $('.top').offset().top
+  }, 1000)
+})
+
+$('.to-bottom-scroll').click(function () {
+  $('html, body').animate({
+    scrollTop: $('.bottom').offset().top
+  }, 1000)
+})
